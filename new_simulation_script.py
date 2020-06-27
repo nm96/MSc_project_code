fc80a78c0c34cc1bb639caf1ce18f98759fa045d
Nico Marrin <as19538@bristol.ac.uk>
2020-06-27 17:00:13 +0100

    Code for basic simulation without any forcing written

--- new_simulation_script.py
+++ new_simulation_script.py
@@ -9,3 +9,33 @@ cos = np.cos
 sin = np.sin
 tanh = np.tanh
 pi = np.pi
+
+def simple(X):
+    return (0,0)
+
+def dXdt(t,X,eps,Om,m,c,k):
+    x,dx,y,dy = X
+    F_x, F_y = 0,0
+    return [dx,
+            (F_x + eps*m*cos(Om*t)*Om**2 - c*dx - k*x)/m,
+            dy,
+            (F_y + eps*m*sin(Om*t)*Om**2 - c*dy - k*y)/m]
+
+eps = 0.03
+Om = 4.1
+m = 10
+c = 0.1
+k = 10
+
+params = (eps,Om,m,c,k)
+    
+tspan = (0,10)    
+tt = np.linspace(*tspan,1000)
+X0 = [0.1,0.2,0.1,0.2]
+
+sol = solve_ivp(dXdt,tspan,X0,t_eval=tt,args=params)
+
+# Plot the trajectory of (x,y)
+plt.plot(sol.y[0],sol.y[2])
+plt.show()
+
