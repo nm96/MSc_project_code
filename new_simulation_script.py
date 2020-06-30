dfa10f17f8374f5fc95e4e94d6fbadf70b94fa56
Nico Marrin <as19538@bristol.ac.uk>
2020-06-30 18:09:40 +0100

    More work on ffts in test script

--- new_simulation_script.py
+++ new_simulation_script.py
@@ -11,50 +11,42 @@ from models import *
 fn = 0  # Initialize figure number for plotting
 
 # Define parameter values
-eps = 0.03  # Rotor eccentricity
-Om = 3    # Driving frequency
-m = 10      # Mass (per unit length)
-c = 0.1     # Damping coefficient
-k = 10      # Stiffness coefficient
-h = 0.03    # Gap width
+eps = 0.0525 # Rotor eccentricity
+Om = 3.5 # Driving frequency
+m = 10 # Mass (per unit length)
+c = 0.05 # Damping coefficient
+k = 10 # Stiffness coefficient
+h = 0.03 # Gap width
 
 model = (simple,(0,)) # Model-tuple for the trivial no-force model
 
 params = (eps,Om,m,c,k,h,model) # Package parameters into a tuple
     
 # Integrate the ODE system over a given time span with given initial conditions
-tspan = (0,100)    
-tt = np.linspace(*tspan,1000)
+
+N = 2**15
+tspan = (0,1000)    
+tt = np.linspace(*tspan,N)
 X0 = [0.1,0,0,0]
 
 sol = solve_ivp(dXdt,tspan,X0,t_eval=tt,args=params)
 
-# Plot the trajectory of (x,y) in the stationary frame
-fn += 1; fig = plt.figure(fn)
-ax = fig.add_axes([.1,.1,.8,.8])
-ax.plot(*solxy(sol))
-
-# Plot the trajectory of (x,y) in a rotating frame
-
-fn += 1; fig = plt.figure(fn)
-ax = fig.add_axes([.1,.1,.8,.8])
-ax.plot(*rotsolxy(sol,Om))
-
 # Plot fft
 
 max_freq = 10
-freq_scale = np.arange(0,max_freq,2*pi/sol.t[-1])
+freq_scale = np.arange(0,max_freq,4*pi/sol.t[-1])
 print(2*pi/sol.t[1])
 
 fn += 1; fig = plt.figure(fn)
 ax = fig.add_axes([.1,.1,.8,.8])
-ax.plot(freq_scale,np.abs(rfft(sol.y[0]))[:len(freq_scale)])
+ax.plot(freq_scale,np.log(np.abs(rfft(sol.y[0][N//2:])))[:len(freq_scale)])
+plt.grid("on")
 
 
 # Integration with VdH model
 
 # Define the model
-k_c = 100 # Required model-specific parameter
+k_c = 50 # Required model-specific parameter
 model = (VdH,(h,k_c)) # This is the standard form for a model;
 # A tuple containing a model function with the form f(X,mparams) and a tuple
 # mparams of model parameters.
@@ -62,31 +54,18 @@ model = (VdH,(h,k_c)) # This is the standard form for a model;
 params = (eps,Om,m,c,k,h,model)
     
 # Integrate the ODE system over a given time span with given initial conditions
-tspan = (0,1000)    
-tt = np.linspace(*tspan,10000)
-X0 = [0.1,0,0,0]
 
 sol = solve_ivp(dXdt,tspan,X0,t_eval=tt,args=params)
-
-# Plot the trajectory of (x,y) in the stationary frame
-fn += 1; fig = plt.figure(fn)
-ax = fig.add_axes([.1,.1,.8,.8])
-ax.plot(*solxy(sol))
-
-# Plot the trajectory of (x,y) in a rotating frame
-
-fn += 1; fig = plt.figure(fn)
-ax = fig.add_axes([.1,.1,.8,.8])
-ax.plot(*rotsolxy(sol,Om))
 
 # Plot fft
 
 max_freq = 10
-freq_scale = np.arange(0,max_freq,2*pi/sol.t[-1])
+freq_scale = np.arange(0,max_freq,4*pi/sol.t[-1])
 print(2*pi/sol.t[1])
 
 fn += 1; fig = plt.figure(fn)
 ax = fig.add_axes([.1,.1,.8,.8])
-ax.plot(freq_scale,np.abs(rfft(sol.y[0]))[:len(freq_scale)])
+ax.plot(freq_scale,np.log(np.abs(rfft(sol.y[0][N//2:])))[:len(freq_scale)])
+plt.grid("on")
 
 plt.show()
