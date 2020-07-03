be2d75b8a6ee9a7c606b07c1de50d3cb8600b8df
Nico Marrin <as19538@bristol.ac.uk>
2020-07-03 12:52:29 +0100

    Adding skeleton of NHSommerfeld model function, updating documentation in models module

--- models.py
+++ models.py
@@ -1,5 +1,10 @@
 """Module containing functions which encode various nonlinear forcing models
-which can be incorporated into the Jeffcott equations."""
+which can be incorporated into the Jeffcott equations.
+
+Each model function takes the phase-space position vector X along with some
+combination of parameters and returns the tuple 
+(fn,ft) = (normal force, tangential force).
+"""
 
 import numpy as np
 
@@ -9,7 +14,7 @@ sin = np.sin
 tanh = np.tanh
 pi = np.pi
 
-def simple(X,dummy):
+def simple(X,dmy):
     """Model function for the trivial no-forcing model. Dummy parameter
     included in order to fit the convention for model-passing.
     Returns the tuple (fn,ft) = (normal force, tangential force).
@@ -26,3 +31,10 @@ def VdH(X,h,k_c):
     r_H = (x*x + y*y)**0.5
     delta_H = (r_H-h)*(tanh(1000*(r_H-h))+1)/2
     return (k_c*delta_H,0)
+
+def NHSommerfeld(X,Omega,h,mu,b,R2):
+    """Model function for the Naive Half-Sommerfeld lubrication model. See
+    preliminary report 3.4.3 for details. Takes global parameters Omega and h
+    along with model-specific parameters mu,b,R2.
+    """
+    pass
