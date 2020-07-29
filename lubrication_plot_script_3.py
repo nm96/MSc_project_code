0bd6050f24a06267c344f16d11b4f53e497c8c9e
Nico Marrin <as19538@bristol.ac.uk>
2020-07-29 11:53:54 +0100

    Producing a beta=1.65 plot

--- lubrication_plot_script_3.py
+++ lubrication_plot_script_3.py
@@ -24,7 +24,7 @@ tanh = np.tanh
 pi = np.pi
 
 # Integration limits
-tspan = (0,2**14)    
+tspan = (0,2**14)  
 N = tspan[1]*2**6
 tt = np.linspace(*tspan,N)
 X0 = [0.01,0,0,0]
@@ -42,12 +42,12 @@ R2 = 100 # Radius
 
 # Special values:
 #c = 0.11
-Om = 3*2**0.5
+Om = 3.7
 
-fn += 1; fig = plt.figure(fn,figsize=[12,7])
+fn += 1; fig = plt.figure(fn,figsize=[12,6])
 spn = 110
 #for B in np.linspace(1.49,1.5,4):
-for B in [0.5]:
+for B in [1.65]:
     Om_nat = (k/m)**0.5 # Shaft natural frequency
     model = (NHSommerfeld2,(Om,h,B))
     params = (eps,Om,m,c,k,model)
@@ -58,25 +58,24 @@ for B in [0.5]:
     ax.set_xlim([0,10])
     ax.axvline(Om_nat,ls='--',c='g',label=r"$\omega_{nat}$")
     ax.axvline(Om,ls='--',c='r',label=r"$\Omega$")
-    om, P = PSD_nw(sol)
-    ax.semilogy(om,P,c='gray',label=r"Spectrum without windowing")
     om, P = PSD(sol)
-    ax.semilogy(om,P,c='k',label=r"Spectrum with Hanning window applied")
-    locmaj = matplotlib.ticker.LogLocator(base=100,numticks=20) 
+    ax.semilogy(om,P,c='k')
+    locmaj = matplotlib.ticker.LogLocator(base=100,numticks=30) 
     ax.yaxis.set_major_locator(locmaj)
     locmin = matplotlib.ticker.LogLocator(base=100,subs=(0.2,0.4,0.6,0.8),numticks=50)
     ax.yaxis.set_minor_locator(locmin)
     ax.yaxis.set_minor_formatter(matplotlib.ticker.NullFormatter())
     ax.grid()
-    ax.set_title(r"""Effect of Windowing ($\beta = {:.2f}$)""".format(B))
+    ax.set_title(r"""$\beta = {:.2f}$""".format(B))
     ax.set_ylabel("$P(\omega)$",rotation=0)
     ax.yaxis.labelpad = 20
-    ax.set_xlabel("$\omega \ (s^{-1})$")
+    ax.set_xlabel("$\omega$")
     ax.legend()
 
 plt.tight_layout()
 
 #fig.savefig("../plots/windowing_effects.eps")
+fig.savefig("../plots/beta_{:.2f}_spectrum.eps".format(B))
 
 tf = time.time()
 print("T = {:.2f}s".format(tf-t0))
