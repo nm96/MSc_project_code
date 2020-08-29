add3f8c26365b8bf8187c04863cf618ddfc95038
Nico Marrin <as19538@bristol.ac.uk>
2020-08-29 09:54:51 +0100

    Trying simulations with zero damping (doesn't work)

--- solver_script.py
+++ solver_script.py
@@ -6,11 +6,12 @@ s1 = Simulation()
 # Set any non-default parameters here:
 
 s1.Om = 1
-s1.B = 15.7
-s1.c = 50
-s1.k = 0.00
+s1.B = 1
+s1.eps = 0.2
+s1.c = 0
+s1.k = 0
 
-s1.T = 2**12
+s1.T = 2**14
 
 # Solve and save data:
 
@@ -19,11 +20,11 @@ s1.solve()
 with open("latest.pkl", 'wb') as f:
     pickle.dump(s1,f,pickle.HIGHEST_PROTOCOL)
 
-subfile = ""
-subfile = "zero_stiffness/"
-afn = "../data/" + subfile + "B{:.3f}_Om{:.2f}_c{:.2f}_T{:.0e}.pkl"
-afn = afn.format(s1.B, s1.Om, s1.c, s1.T)
-
-if s1.T > 2**10:
-    with open(afn, 'wb') as f:
-        pickle.dump(s1,f,pickle.HIGHEST_PROTOCOL)
+#subfile = ""
+#subfile = "zero_stiffness/"
+#afn = "../data/" + subfile + "B{:.3f}_Om{:.2f}_c{:.2f}_T{:.0e}.pkl"
+#afn = afn.format(s1.B, s1.Om, s1.c, s1.T)
+#
+#if s1.T > 2**10:
+#    with open(afn, 'wb') as f:
+#        pickle.dump(s1,f,pickle.HIGHEST_PROTOCOL)
