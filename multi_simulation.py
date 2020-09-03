6d049b8168caaf5accd43af55828eb06fd3c4c43
Nico Marrin <as19538@bristol.ac.uk>
2020-09-03 16:46:27 +0100

    Alternate multi-simulation

--- multi_simulation.py
+++ multi_simulation.py
@@ -4,15 +4,27 @@ import pickle
 
 periods = []
 
-c_set = np.linspace(0.2,0.6,21)
-for c in c_set:
+#c_set = np.linspace(0.2,0.6,21)
+#for c in c_set:
+#    s = simulation.Simulation()
+#    s.B = 1.65
+#    s.c = c
+#    s.T = 2**12
+#    s.solve()
+#    s.find_period()
+#    periods.append(s.Tp)
+#
+#for k in range(len(c_set)):
+#    print("c = {:.2f} --> Tp = {:.4f}".format(c_set[k], periods[k]))
+
+B_set = np.linspace(1.3,1.7,21)
+for B in B_set:
     s = simulation.Simulation()
-    s.B = 1.65
-    s.c = c
+    s.B = B
     s.T = 2**12
     s.solve()
-    s.find_period()
+    s.find_period
     periods.append(s.Tp)
 
 for k in range(len(c_set)):
-    print("c = {:.2f} --> Tp = {:.4f}".format(c_set[k], periods[k]))
+    print("B = {:.2f} --> Tp = {:.4f}".format(B_set[k], periods[k]))
